128b869e407349a374c053e431fa9a753043722b
siryoav <siryoav@gmail.com>
2021-01-10 10:42:38 +0200

    validate 10 digits isbn

--- T1/src/com/yoav/isbn/ValidateISBN.java
+++ T1/src/com/yoav/isbn/ValidateISBN.java
@@ -3,6 +3,7 @@ package com.yoav.isbn;
 public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
+		if(isbn.length() != 10) throw new NumberFormatException("Not 10 digits");
 		int total = 0;
 		for (int i = 0; i < 10; i++) {
 			total += isbn.charAt(i) * (10 - i);
