6a5a42552dea926bd220ad4c419ab33f987b33c6
siryoav <siryoav@gmail.com>
2021-01-10 13:49:23 +0200

    add db/web ISBN service

--- T1/src/com/yoav/isbn/StockManager.java
+++ T1/src/com/yoav/isbn/StockManager.java
@@ -1,15 +1,23 @@
 package com.yoav.isbn;
 
 public class StockManager {
+
+	private ExternalISBNDataService webService;
+	private ExternalISBNDataService dbService;
 	
-	private ExternalISBNDataService service;
+	public void setWebService(ExternalISBNDataService service) {
+		this.webService = service;
+	}
 	
-	public void setService(ExternalISBNDataService service) {
-		this.service = service;
+	public void setDBService(ExternalISBNDataService service) {
+		this.dbService = service;
 	}
 
 	public String getLocatorCode(String isbn) {
-		Book book = service.lookup(isbn);
+		Book book = dbService.lookup(isbn);
+		if(book == null) {			
+			book = webService.lookup(isbn);
+		}
 		StringBuilder locator = new StringBuilder();
 		locator.append(isbn.substring(isbn.length() - 4));
 		locator.append(book.getAutor().substring(0,1));
--- T1/test/com/yoav/isbn/StockManagementTests.java
+++ T1/test/com/yoav/isbn/StockManagementTests.java
@@ -8,15 +8,24 @@ class StockManagementTests {
 
 	@Test
 	void testCanGetACorrectLocatorCode() {
-		ExternalISBNDataService testService = new ExternalISBNDataService() {
+		ExternalISBNDataService testWebService = new ExternalISBNDataService() {
 			
 			@Override
 			public Book lookup(String isbn) {
 				return new Book(isbn, "Of Mice And Men", "J. Stenbeck");
 			}
 		};
+		
+		ExternalISBNDataService testDBService = new ExternalISBNDataService() {
+			
+			@Override
+			public Book lookup(String isbn) {
+				return null;
+			}
+		};
 		StockManager stockManager = new StockManager();
-		stockManager.setService(testService);
+		stockManager.setWebService(testWebService);
+		stockManager.setDBService(testDBService);
 		
 		String isbn = "0140177396";
 		String locatorCode = stockManager.getLocatorCode(isbn);
